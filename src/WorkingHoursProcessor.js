3886ac64d34c7e31fd34933621a4ba5db97b7339
Siphenathi Pantshwa <SiphenathiP@SAHOMELOANS.COM>
2018-06-12 16:22:50 +0200

    Completed with 1 requirement that needs to be added

--- src/WorkingHoursProcessor.js
+++ src/WorkingHoursProcessor.js
@@ -1,26 +1,70 @@
 function WorkingHoursProcessor() {
-    
-let _days = Getdays();
 
+    let _days = Getdays();
     return {
         GetFormatedReadableHours: function (data) {
+            let results = '';
             if (data.length == 0) {
-                return [];
+                return data;
             }
-            return data.length;
+            if (data.length == 1) {
+
+                return `${data[0].day.toUpperCase()}: ${data[0].from} - ${data[0].to}`;
+            }
+            else {
+                let ArrayWithListOfReadableWorkingHours = [];
+
+                for (let x = 0; x < _days.length; x++) {
+                    let currentDay = data.find(a => a.day == _days[x].day);
+
+
+                    if (currentDay != null) {
+                        let dayId = _days[x].id;
+                        let objectWithReadableWorkingHours = { id: dayId, day: currentDay.day.toUpperCase(), hours: currentDay.from + ' - ' + currentDay.to };
+
+                        if (ArrayWithListOfReadableWorkingHours.length == 0) {
+                            ArrayWithListOfReadableWorkingHours.push(objectWithReadableWorkingHours);
+                        }
+                        else {
+                            let lastRecordIndex = ArrayWithListOfReadableWorkingHours.length - 1;
+                            let lastRecord = ArrayWithListOfReadableWorkingHours[lastRecordIndex]
+
+                            if ((dayId - 1) == lastRecord.id && lastRecord.hours == (currentDay.from + ' - ' + currentDay.to)) {
+
+                                ArrayWithListOfReadableWorkingHours[lastRecordIndex].id = dayId;
+                                ArrayWithListOfReadableWorkingHours[lastRecordIndex].day = lastRecord.day + " - " + currentDay.day.toUpperCase();
+                            }
+                            else {
+                                ArrayWithListOfReadableWorkingHours.push(objectWithReadableWorkingHours);
+                            }
+                        }
+                    }
+                }
+                results = GetResultsAsString(ArrayWithListOfReadableWorkingHours);
+            }
+            return results;
         }
     }
 
-     function Getdays(){
+    function GetResultsAsString(ArrayWithListOfReadableWorkingHours) {
+        let result = '';
+        for (let x = 0; x < ArrayWithListOfReadableWorkingHours.length; x++) {
 
-        let days = [{'day':'mon'},
-                    {'day':'tue'},
-                    {'day':'wed'},
-                    {'day':'thur'},
-                    {'day':'fri'},
-                    {'day':'sat'},
-                    {'day':'sun'}];
+            result += `${ArrayWithListOfReadableWorkingHours[x].day}: ${ArrayWithListOfReadableWorkingHours[x].hours}\n`;
+
+        }
+        return result.trim();
+    }
+
+    function Getdays() {
+
+        let days = [{ 'id': 1, 'day': 'mon' },
+        { 'id': 2, 'day': 'tue' },
+        { 'id': 3, 'day': 'wed' },
+        { 'id': 4, 'day': 'thu' },
+        { 'id': 5, 'day': 'fri' },
+        { 'id': 6, 'day': 'sat' },
+        { 'id': 7, 'day': 'sun' }];
         return days;
-                
     }
 }
--- test/WorkingHoursProcessorTest.spec.js
+++ test/WorkingHoursProcessorTest.spec.js
@@ -13,23 +13,82 @@ describe("WorkingHoursProcessor", function () {
             })
         })
 
-        describe("Given an array of objects with values", function () {
-            it("Should return length of array to confirm if array is not empty", function () {
+        // describe("Given an array of objects with values", function () {
+        //     it("Should return length of array to confirm if array is not empty", function () {
+        //         //Arrange
+        //         let sut = new WorkingHoursProcessor();
+        //         let data = [{"day": "sat", "from": "10:00", "to": "23:00"},
+        //                     {"day": "mon", "from": "11:00", "to": "23:00"},
+        //                     {"day": "tue", "from": "11:00", "to": "23:00"},
+        //                     {"day": "wed", "from": "11:00", "to": "23:00"},
+        //                     {"day": "thu", "from": "12:00", "to": "23:00"},
+        //                     {"day": "fri", "from": "12:00", "to": "23:00"},
+        //                     {"day": "sun", "from": "11:00", "to": "23:00"}
+        //                    ];
+        //         //Act
+        //         let actual = sut.GetFormatedReadableHours(data);
+        //         //Assert
+        //         expect(actual).toEqual(7);
+        //     })
+        // })
+
+        describe("Given an array with working hours of 1 day", function () {
+            it("Should return 1 day in a human readable format ", function () {
                 //Arrange
                 let sut = new WorkingHoursProcessor();
-                let data = [{"day": "sat", "from": "10:00", "to": "23:00"},
-                            {"day": "mon", "from": "11:00", "to": "23:00"},
-                            {"day": "tue", "from": "11:00", "to": "23:00"},
-                            {"day": "wed", "from": "11:00", "to": "23:00"},
-                            {"day": "thu", "from": "12:00", "to": "23:00"},
-                            {"day": "fri", "from": "12:00", "to": "23:00"},
-                            {"day": "sun", "from": "11:00", "to": "23:00"}
-                           ];
+                let data = [{"day": "mon", "from": "11:00", "to": "23:00"}];
 
                 //Act
                 let actual = sut.GetFormatedReadableHours(data);
                 //Assert
-                expect(actual).toEqual(7);
+                expect(actual).toEqual("MON: 11:00 - 23:00");
+            })
+        })
+
+        describe("Given an array of 2 consecutive days with same working hours", function () {
+            it("Should return 2 consecutive days in a human readable format", function () {
+                //Arrange
+                let sut = new WorkingHoursProcessor();
+                let data = [{"day": "mon", "from": "11:00", "to": "23:00"},
+                            {"day": "tue", "from": "11:00", "to": "23:00"}];
+
+                //Act
+                let actual = sut.GetFormatedReadableHours(data);
+                //Assert
+                expect(actual).toEqual("MON - TUE: 11:00 - 23:00");
+            })
+        })
+
+        describe("Given an array of 2 consecutive days and 1 non-consecutive day with same working hours", function () {
+            it("Should return 2 consecutive days and 1 non-consecutive day in a human readable format", function () {
+                //Arrange
+                let sut = new WorkingHoursProcessor();
+                let data = [{"day": "mon", "from": "11:00", "to": "23:00"},
+                            {"day": "tue", "from": "11:00", "to": "23:00"},
+                            {"day": "fri", "from": "11:00", "to": "23:00"}
+                            ];
+
+                //Act
+                let actual = sut.GetFormatedReadableHours(data);
+                //Assert
+                expect(actual).toEqual("MON - TUE: 11:00 - 23:00\nFRI: 11:00 - 23:00");
+            })
+        })
+
+        describe("Given an array of 3 consecutive days and 1 non-consecutive day with same working hours", function () {
+            it("Should return 3 consecutive days and 1 non-consecutive day in a human readable format", function () {
+                //Arrange
+                let sut = new WorkingHoursProcessor();
+                let data = [{"day": "mon", "from": "11:00", "to": "23:00"},
+                            {"day": "tue", "from": "11:00", "to": "23:00"},
+                            {"day": "wed", "from": "11:00", "to": "23:00"},
+                            {"day": "fri", "from": "11:00", "to": "23:00"}
+                            ];
+
+                //Act
+                let actual = sut.GetFormatedReadableHours(data);
+                //Assert
+                expect(actual).toEqual("MON - TUE - WED: 11:00 - 23:00\nFRI: 11:00 - 23:00");
             })
         })
 
